ce8bcf9f746f6f11013bc3e40942e56252f8d316
riddhi soni <riddhisoni2020@gmail.com>
2024-05-20 10:42:55 +0530

    Initial commit

---
+++ firstclass.java
@@ -0,0 +1,5 @@
+class first class{
+    public static void main(strings args []){
+        FileSystem.out.println("hello world");
+    }
+}
---
+++ firstprogram.py
@@ -0,0 +1,3 @@
+print("my name is riddhi")
+print(20)
+print("hello world")

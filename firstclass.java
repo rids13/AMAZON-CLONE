d2bfb8175b5fa78c04f683e4f31d537b01c1246c
riddhi soni <riddhisoni2020@gmail.com>
2024-05-23 20:37:37 +0530

    add an title

--- firstclass.java
+++
@@ -1,5 +0,0 @@
-class first class{
-    public static void main(strings args []){
-        FileSystem.out.println("hello world");
-    }
-}
--- firstprogram.py
+++
@@ -1,3 +0,0 @@
-print("my name is riddhi")
-print(20)
-print("hello world")
